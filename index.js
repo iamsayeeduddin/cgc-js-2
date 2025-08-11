3efd117de5863800a9e0635b1a2a96301ea50e13
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-08-11 08:28:09 +0530

    JS Init

---
+++ index.js
@@ -0,0 +1,37 @@
+// VARIABLES
+
+var fullName = "Syed Sayeeduddin";
+fullName = 78;
+// console.log(fullName);
+var a = 10;
+
+var fullName = "Rest";
+// console.log(fullName);
+
+var fullname = "Sayeed";
+// console.log(fullName, fullname);
+
+// Declaration -> Initialization
+var test;
+// console.log(test);
+
+// Data Types
+// String
+// console.log(typeof `132131231323`);
+// Number
+// console.log(typeof 1);
+// Boolean
+// console.log(false);
+// Functions
+// function printName() {
+//   console.log("Hello User!");
+// }
+
+// printName();
+// Arrays
+// [1, 2, 2, 3, 4, 4, 44, 4, 4, 5];
+// Objects
+// {
+//   name: "Sayeed";
+// }
+// undefined, NaN, null
