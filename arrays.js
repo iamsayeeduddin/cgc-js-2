8c79349e1e1b5c7c858f7cf9cf945d2d038ded82
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-08-18 08:08:19 +0530

    Arrays & Loops

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var arr = ["Sayeed", "Ahmed", "Ishaq", "Sayeed"];
+// console.log(arr[1]);
+arr[1] = "Essam";
+// console.log(arr.length);
+arr.push("Hassan");
+// arr.pop();
+arr.unshift("Azam");
+// arr.shift();
+// console.log(typeof arr); // object
+// console.log(Array.isArray({}));
+// console.log(arr);
+// console.log(arr.slice(1, 4), arr);
+// console.log(arr.join("/"));
+// console.log(arr.includes("sayeed"));
+// console.log(arr.indexOf("Sayeed"));
+// console.log(arr.lastIndexOf("Sayeed"));
