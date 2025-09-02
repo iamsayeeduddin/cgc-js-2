6675f4633626ba55c75c85fe8188d6df1aa4b240
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-02 07:46:12 +0530

    Async JS 2

---
+++ async1.js
@@ -0,0 +1,51 @@
+const add = (a, b) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log("In ADD ASYNC");
+      if (a === 0) return rej("0 not allowed");
+      res(a + b);
+    }, 1000);
+  });
+};
+
+const subtract = (a, b) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log("In SUBTRACT ASYNC");
+      if (a === 0) return rej("0 not allowed");
+      res(a - b);
+    }, 2000);
+  });
+};
+
+const multiply = (a, b) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log("In <MULTIPLY> ASYNC");
+      if (a === 25) return rej("25 not allowed");
+      res(a * b);
+    }, 1000);
+  });
+};
+
+// add(0, 20)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// subtract(10, 20)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// multiply(3, 20)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+// const prms = new Promise((res, rej) => res(10));
+
+// prms.then((res) => console.log(res));
+
+add(10, 30)
+  .then((res) => subtract(res, 5))
+  .then((res2) => multiply(res2, 2))
+  .then((res3) => console.log(res3))
+  .catch((err) => console.log(err))
+  .finally(() => console.log("Promise Ended!"));
